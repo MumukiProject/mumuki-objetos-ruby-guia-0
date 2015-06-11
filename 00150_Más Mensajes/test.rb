c97347f51b7034a92f64050fddc491248736d6d2
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-11 18:18:20 -0300

    Typo

--- 00150_Más Mensajes/test.rb
+++ 00150_Más Mensajes/test.rb
@@ -1,5 +1,5 @@
-describe "pepita" do
+describe "anastasia" do
   it('se le envio el mensaje cantar exactamente una vez') do
-      expect(pepita.canto).to eq 1
+      expect(anastasia.canto).to eq 1
   end
 end
