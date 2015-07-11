589a9479b44543ae14b6b238654acc4acf9c9140
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-07-11 16:07:35 -0300

    Corrigiendo titulo

--- 00130_Métodos, Primera Parte/test.rb
+++ 00130_Métodos, Primera Parte/test.rb
@@ -5,7 +5,7 @@ describe "pepita" do
   it('es un objeto') do
       expect(pepita.class).to be Object
   end
-  it('anasitasia.cantar! devuelve "pri"') do
+  it('pepita.cantar! devuelve "pri"') do
       expect(pepita.cantar!).to eq "pri pri priiiii"
   end
 end
